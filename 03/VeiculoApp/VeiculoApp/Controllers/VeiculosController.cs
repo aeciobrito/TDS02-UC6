9534230d95caea876fc5b23e57a91acedc98ab1e
Aecio Brito <aeciobrito@gmail.com>
2025-10-21 22:16:39 -0300

    Aula 03 - Projeto Veiculos

---
+++ 03/VeiculoApp/VeiculoApp/Controllers/VeiculosController.cs
@@ -0,0 +1,35 @@
+﻿using VeiculoApp.Data;
+using VeiculoApp.Models;
+
+namespace VeiculoApp.Controllers
+{
+    internal class VeiculosController
+    {
+        private AppDbContext _context;
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Adicionar()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Adicionar Veículo ===");
+            Console.Write("Marca: ");
+            string marca = Console.ReadLine() ?? string.Empty;
+
+            Console.Write("Modelo: ");
+            string modelo = Console.ReadLine() ?? string.Empty;
+
+            var veiculo = new Veiculo
+            {
+                Marca = marca,
+                Modelo = modelo
+            };
+
+            _context.Veiculos.Add(veiculo);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ 03/VeiculoApp/VeiculoApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using VeiculoApp.Data;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        string connectionString = "Server=(localdb)\\mssqllocaldb;Database=VeiculosAppDb;Trusted_Connection=True;";
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connectionString));
+    })
+    .Build();
+
+
